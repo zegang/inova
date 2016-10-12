2d7cd0623dc169217b354797339c9f3e97aa1893
zegang <zegang.luo@qq.com>
2016-10-12 20:08:46 +0800

    tabs avaliable, modle form added

--- inova.js
+++ inova.js
@@ -1,5 +1,7 @@
 function getDevices(){
-    $.getJSON("http://localhost:8080/devices", function(result) {
+    var url = "http://localhost:8080/devices";
+    //var url = "http://146.11.25.2:8010/devices";
+    $.getJSON(url, function(result) {
             var devices = result;
             showDevices(devices);
             });
@@ -14,15 +16,17 @@ function getDockerImages(){
 
 function showDevices(devices){
     for (var i=0;i<devices.length;i++){
+        var hwinfoTab = "hwinfo_"+devices[i].name;
+        var imgTab = "images_"+devices[i].name; 
         var devicesHtml = '<div id="'+ devices[i].uuid +'" class="col-xs-3 col-sm-3 col-lg-3 panel panel-info'; if(i>0) devicesHtml += ' col-md-offset-1">';
         else devicesHtml += '">';
         devicesHtml += '<div class="panel-heading"><h3 class="panel-title">'+ devices[i].name +'</h3></div>' +
             '<table class="table table-striped"><tbody>'+
             '<tr class="row"><td class="col-xs-4 col-sm-4 col-lg-4">UUID</td><td class="col-xs-8 col-sm-8 col-lg-8">' + devices[i].uuid + '</td></tr>' +
             '</tbody></table>' +
-            '<ul class="nav nav-tabs" role="tablist"><li role="presentation" class="active"><a href="#hwinfo" role="tab" aria-controls="hwinfo" aria-expanded="true">HWInfo</a></li><li role="presentation"><a href="#images" role="tab">Images</a></li></ul>' +
+            '<ul class="nav nav-tabs" role="tablist"><li role="presentation" class="active"><a href="#' + hwinfoTab +'" role="tab" aria-controls="'+hwinfoTab +'" aria-expanded="true">HWInfo</a></li><li role="presentation"><a href="#' + imgTab +'" role="tab">Images</a></li></ul>' +
             '<div class="tab-content">'+
-            '<div id="hwinfo" class="tab-pane fade active in" role="tabpanel" aria-labelledby="hwinfo-tab">'+
+            '<div id="' + hwinfoTab +'" class="tab-pane fade active in" role="tabpanel" aria-labelledby="' + hwinfoTab +'-tab">'+
             '<table class="table table-striped"><tbody>'+
             '<tr class="row"><td class="col-xs-4 col-sm-4 col-lg-4">Host:</td><td class="col-xs-8 col-sm-8 col-lg-8">' + devices[i]["HWInfo"].address.host + '</td></tr>' +
             '<tr class="row"><td class="col-xs-4 col-sm-4 col-lg-4">Ports:</td> <td class="col-xs-8 col-sm-8 col-lg-8">' + devices[i]["HWInfo"].address.port + '</td></tr>' +
@@ -30,13 +34,15 @@ function showDevices(devices){
             '<tr class="row"><td class="col-xs-4 col-sm-4 col-lg-4">CPU:</td> <td class="col-xs-8 col-sm-8 col-lg-8">' + devices[i]["HWInfo"].capacity.cpu + '</td></tr>' +
             '</tbody></table>'+
             '</div>'+
-            '<div id="images" class="tab-pane fade" role="tabpanel" aria-labelledby="images-tab">'+
+            '<div id="' + imgTab + '" class="tab-pane fade" role="tabpanel" aria-labelledby="' + imgTab + '-tab">'+
             '<div class="row"><div class="col-xs-12">'+ dockersHtml(devices[i].Images) + '</div></div>'+
             '</div>'+
             '</div>'+
             '</div>';
         $("#devicesContainer").append(devicesHtml);
     }
+
+    $('.nav-tabs a').click(function(e){ e.preventDefault();$(this).tab('show')});
 }
 
 function dockersHtml(dockers){
@@ -44,7 +50,7 @@ function dockersHtml(dockers){
     dockersTable += '<tbody class="docimgs">';
     var i=1;
     for(var j in dockers){
-        dockersTable += '<tr><td>'+ (i++) +'</td><td>'+ dockers[j].name +'</td><td>'+ dockers[j].url +'</td><td>'+ dockers[j].Config +'</td><td><button type="button" class="btn"><span class="glyphicon glyphicon-cog"></span></button></td></tr>';
+        dockersTable += '<tr><td>'+ (i++) +'</td><td>'+ dockers[j].name +'</td><td>'+ dockers[j].url +'</td><td>'+ dockers[j].Config +'</td><td><button class="img-op-btn" data-toggle="modal" data-target="#myModal" type="button"><span class="glyphicon glyphicon-cog"></span></button></td></tr>';
     }
     dockersTable += '<tr ondragover="allowDrop(event)" ondrop="drop(event)"><td colspan="5"><button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-plus-sign"> Drag to add docker images</span></button></td></tr>';
     dockersTable += '</tbody></table>';
@@ -66,3 +72,6 @@ function showDockerImages(dockerimages){
     }
 }
 
+function creatModalForm(){
+
+}
